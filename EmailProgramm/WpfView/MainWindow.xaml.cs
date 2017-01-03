ba56d36c6a1901bc15b4b64dba98a82b72a46469
Daniel Glaser <danielglaser9@gmail.com>
2017-01-03 17:22:39 +0100

    Send Email Fenster öffnet sich.

--- EmailProgramm/WpfView/MainWindow.xaml.cs
+++ EmailProgramm/WpfView/MainWindow.xaml.cs
@@ -24,5 +24,11 @@ namespace WpfView
         {
             InitializeComponent();
         }
+
+        private void SendEmail_Click(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("SendEmail_Click");
+            new SendEmailWindow();
+        }
     }
 }
