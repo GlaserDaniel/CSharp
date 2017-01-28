2c18a4c7ce188edc2244311fde54f18f4469756e
Daniel Glaser <danielglaser9@gmail.com>
2017-01-28 19:30:25 +0100

    Kleinigkeiten Serilizable bei Exceptions hinzugefügt. Speichern Buttons links gemacht. Anhänge nun horizontal.

--- EmailProgramm/Common/Others/Ecxeptions.cs
+++ EmailProgramm/Common/Others/Ecxeptions.cs
@@ -2,23 +2,33 @@
 
 namespace Common.Exceptions
 {
+    [Serializable]
     public class IMAPPOP3PortEmptyException : Exception { }
 
+    [Serializable]
     public class SMTPPortEmtpyException : Exception { }
 
+    [Serializable]
     public class IMAPPOP3PortFormatException : Exception { }
 
+    [Serializable]
     public class SMTPPortFormatException : Exception { }
 
+    [Serializable]
     public class ShownameEmptyException : Exception { }
 
+    [Serializable]
     public class UserEmptyException : Exception { }
 
+    [Serializable]
     public class EmailEmptyException : Exception { }
 
+    [Serializable]
     public class PasswordEmptyException : Exception { }
 
+    [Serializable]
     public class IMAPPOP3ServerEmptyException : Exception { }
 
+    [Serializable]
     public class SMTPServerEmptyException : Exception { }
 }
